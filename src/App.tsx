d91afef019f649da57ec47658073f8d1e4819dce
Oleksii Aheiev <aheiev.oleksii@gmail.com>
2025-11-04 15:04:25 +0200

    start project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.scss';
+import {Routes} from 'react-router';
+
+export default function App() {
+    return (
+        <>
+            <Routes>
+            </Routes>
+        </>
+    )
+}
